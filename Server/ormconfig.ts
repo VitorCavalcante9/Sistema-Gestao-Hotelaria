60e113299125faebae8bf9e10be21f80c752ca39
Vitor Cavalcante <v9cavalcante@gmail.com>
2022-12-13 18:14:46 -0300

    inicializacao do servidor

---
+++ Server/ormconfig.ts
@@ -0,0 +1,13 @@
+export default {
+  type: process.env.DB_CONNECTION,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  migrations: ['./src/database/migrations/**.ts'],
+  entities: ['./src/models/**.ts'],
+  cli: {
+    migrationsDir: './src/database/migrations',
+  },
+};
